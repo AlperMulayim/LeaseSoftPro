d24b704b913b645c6e8778c5d08316c65706bb93
alpermulayim <alper.mulayim48@gmail.com>
2022-05-10 16:47:10 +0300

    project init

---
+++ leasesoftpro-be/src/main/java/com/alper/leasesoftprobe/LeasesoftproBeApplication.java
@@ -0,0 +1,13 @@
+package com.alper.leasesoftprobe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeasesoftproBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeasesoftproBeApplication.class, args);
+	}
+
+}
