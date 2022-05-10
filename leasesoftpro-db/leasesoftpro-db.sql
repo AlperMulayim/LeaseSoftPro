736bafbf57a37a9969d8b6243a31122fc6a39084
alpermulayim <alper.mulayim48@gmail.com>
2022-05-10 18:21:03 +0300

    DB tables create init

---
+++ leasesoftpro-db/leasesoftpro-db.sql
@@ -0,0 +1,81 @@
+CREATE  TABLE mytable
+(
+    id INT AUTO_INCREMENT,
+    myname VARCHAR(10),
+    PRIMARY KEY ( id )
+
+);
+
+
+CREATE TABLE lesapro_building_adress(
+    id INT NOT NULL AUTO_INCREMENT,
+    street VARCHAR(100),
+    city VARCHAR(100),
+    building_number INT,
+    block_number INT,
+    site_name VARCHAR(100),
+    PRIMARY KEY(id)
+);
+
+
+CREATE TABLE lesapro_buildings(
+    id INT NOT NULL AUTO_INCREMENT,
+    buiding_name VARCHAR(100) NOT NULL,
+    standart_price_permonth DOUBLE,
+    fee_permonth DOUBLE,
+    init_date DATE,
+    adress_id INT,
+
+    PRIMARY KEY(id),
+    FOREIGN KEY(adress_id) REFERENCES lesapro_building_adress(id)
+);
+
+CREATE TABLE lesapro_floors(
+    id INT NOT NULL AUTO_INCREMENT,
+    total_unit_per_floor INT,
+    building_id INT,
+    floor_number INT NOT NULL,
+    
+    PRIMARY KEY(id),
+    FOREIGN KEY(building_id) REFERENCES lesapro_buildings(id)
+);
+
+
+INSERT INTO lesapro_building_adress(
+    street,
+    city
+) VALUES(
+    "ABC STREET",
+    "MYCITYAWASOME"
+);
+
+INSERT INTO lesapro_buildings (
+    buiding_name,
+    adress_id,
+    fee_permonth,
+    standart_price_permonth,
+    init_date
+)
+VALUES(
+    "LESA RESIDENCE",
+    1,
+    200.0,
+    1000.0, 
+    curdate()
+ );
+
+INSERT INTO lesapro_floors(
+    total_unit_per_floor,
+    building_id,
+    floor_number
+) VALUES(
+    4,1,2
+);
+
+ SELECT * FROM lesapro_buildings;
+ SELECT * FROM lesapro_building_adress;
+ SELECT * FROM lesapro_floors;
+
+DROP TABLE lesapro_buildings;
+DROP TABLE lesapro_building_adress;
+DROP TABLE lesapro_floors;
