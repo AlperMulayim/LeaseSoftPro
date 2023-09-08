1a240323b34169870d56b0c7449b06034ddac387
alpermulayim <alper.mulayim48@gmail.com>
2023-09-08 21:36:48 +0300

    DB - Employees table and insert added.

--- leasesoft-db/leasesoft-db.sql
+++ leasesoft-db/leasesoft-db.sql
@@ -1,4 +1,5 @@
--- Active: 1664392447753@@127.0.0.1@3306
+-- Active: 1694183684972@@127.0.0.1@3306@leasesoftv2
+
 CREATE DATABASE IF NOT EXISTS leasesoftv2;
 USE leasesoftv2;
 
@@ -10,5 +11,16 @@ CREATE TABLE  ldb_client(
     email VARCHAR(255),
     phone VARCHAR(255),
     create_date DATE
+);
+
+CREATE TABLE  ldb_employees(  
+    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    first_name VARCHAR(255),
+    surname VARCHAR(255),
+    email VARCHAR(255),
+    phone VARCHAR(255),
+    start_date DATE,
+    end_date DATE,
+    job_title VARCHAR(255)
 );
 
