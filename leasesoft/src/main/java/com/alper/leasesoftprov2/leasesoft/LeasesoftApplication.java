907497372ddbf629c876723c66ca6da4ed2271f2
alpermulayim <alper.mulayim48@gmail.com>
2023-09-08 17:27:54 +0300

    V2 leasesoft-be initialized

---
+++ leasesoft/src/main/java/com/alper/leasesoftprov2/leasesoft/LeasesoftApplication.java
@@ -0,0 +1,13 @@
+package com.alper.leasesoftprov2.leasesoft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeasesoftApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeasesoftApplication.class, args);
+	}
+
+}
---
+++ leasesoft/src/test/java/com/alper/leasesoftprov2/leasesoft/LeasesoftApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alper.leasesoftprov2.leasesoft;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeasesoftApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
