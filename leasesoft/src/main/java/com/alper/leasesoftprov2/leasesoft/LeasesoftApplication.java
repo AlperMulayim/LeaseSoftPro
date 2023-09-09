a34e7b23091564d71b496f81fb833c28ad96019e
alpermulayim <alper.mulayim48@gmail.com>
2023-09-09 20:44:52 +0300

    Spring Doc. swagger added.

--- leasesoft/src/main/java/com/alper/leasesoftprov2/leasesoft/LeasesoftApplication.java
+++ leasesoft/src/main/java/com/alper/leasesoftprov2/leasesoft/LeasesoftApplication.java
@@ -3,6 +3,7 @@ package com.alper.leasesoftprov2.leasesoft;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class LeasesoftApplication {
 
